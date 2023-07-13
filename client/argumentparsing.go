57eb4204eed22dcf10ea2ed6d283db9d8921f02a
Clayton Townsend II <clayton@claytontii.com>
2023-07-13 14:13:53 -0500

    client: treat a no such file error in config loading to be a non-fatal error

--- client/argumentparsing.go
+++ client/argumentparsing.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -59,12 +60,14 @@ func parseCmdlineArguments() (*config, error) {
 
 	// Unmarshal JSON from config.json
 	bs, err := os.ReadFile(filepath.Join(args.Dir, "config.json"))
-	if err != nil {
+	switch {
+	case errors.Is(err, fs.ErrNotExist):
+	case err != nil:
 		return nil, fmt.Errorf("could not ReadFile (%w)", err)
-	}
-
-	if err = json.Unmarshal(bs, &args); err != nil {
-		return nil, fmt.Errorf("could not unmarshal config.json as JSON error (%w)", err)
+	default:
+		if err = json.Unmarshal(bs, &args); err != nil {
+			return nil, fmt.Errorf("could not unmarshal config.json as JSON error (%w)", err)
+		}
 	}
 
 	// Server
